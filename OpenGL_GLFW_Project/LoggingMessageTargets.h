023c076b25fbe51bc6a5d2af52ab9fc56c39b737
Forrest S Miller <4estmiller@comcast.net>
2019-06-22 23:46:14 -0700

    Rewrote the comment header on the header 'LoggingMessageTargets.h' to provide a description of how logging has been implemented and some review over how it's underlying <cstdio> functionality behaves.

--- OpenGL_GLFW_Project/LoggingMessageTargets.h
+++ OpenGL_GLFW_Project/LoggingMessageTargets.h
@@ -1,8 +1,56 @@
-//For logging 
 //
-//   Since the vast majority of other files in this project will use logging, 
-//   it just makes sense to put these in their own file.
-
+// File:                        LoggingMessageTargets.h
+//
+// How Logging Works:               Logging in this project is very rudimentary, with pretty
+//                              much everything being handle through the C function 'fprintf()'.
+//
+//                                  The syntax of 'fprintf()' is very similar to 'printf()'s 
+//                              syntax, with the only difference being 'fprintf()' takes an
+//                              additional first parameter which represents any open FILE*
+//                              that identifies as an output stream.  
+//                                      Syntax:
+//                                           int fprintf ( FILE * stream, const char * format, ... );
+//                             
+//                                 [The integer value returned by 'fprintf()' represents the number of 
+//                              characters written to the stream, but this value is typically ignored]
+//                              
+//                                  Every C/C++ program by default has available to it the following
+//                              three FILE* streams: 
+//                                                     (i)   stdin
+//                                                    (ii)   stdout
+//                                                   (iii)   stderr
+//                              The first stream 'stdin' is not an output stream, so it plays no further
+//                              roll in this discussion. 
+//                              The second stream 'stdout' is meant as the default output destination for 
+//                              an Application. While its implementation is system-dependent, it typically
+//                              by defaults targets the text console. However, it is possible on most Operating
+//                              Systems for 'stdout' to be redirected at the time of Application invocation.
+//                                 (e.g. most shells support syntax such as:    myapplication > example.txt   
+//                                    This redirects any text written to 'stdout' to instead be written to
+//                                    the file 'example.txt')
+//                              The final stream is 'stderr'. It is just like 'stdout', except its default 
+//                              buffering behavior is more restricted. 'stderr' is guaranteed to be either
+//                              unbuffered or line-buffered, while implementations are allowed to implement
+//                              a more involved full-buffering on 'stdout'.
+//                              
+// The Log Target Macros:       There are currently 3 targets that this Application uses for logging itself. 
+//                              These targets have been used throughout as the following 3 macros: 
+//                                                   MSGLOG              [Message Log]
+//                                                   WRNLOG              [Warning Log]
+//                                                   ERRLOG              [ERROR LOG]
+//                              Because these three targets are implemented as macros, it is possible to 
+//                              easily modify logging behavior for the entire project simply by redefining 
+//                              which FILE* they represent. 
+// References:                 
+//                   //fprintf()
+//                                 http://www.cplusplus.com/reference/cstdio/fprintf/
+//                   //stdout
+//                                 http://www.cplusplus.com/reference/cstdio/stdout/
+//                   //stderr
+//                                 http://www.cplusplus.com/reference/cstdio/stderr/
+//                   //Change a FILE* buffering properties
+//                                 http://www.cplusplus.com/reference/cstdio/setvbuf/ 
+//
 //July and August 2018          Forrest Miller
 
 #pragma once
@@ -32,41 +80,5 @@
 #define WRNLOG stderr
 #endif 
 
-
-
-
-////////////////////////////////////////
-////        Case Enum Logger        ////  
-////////////////////////////////////////
-
-//First we need a macro to print the enum
-//This is based off of the top answer at ////see top answer at https://stackoverflow.com/questions/9907160/how-to-convert-enum-names-to-string-in-c
-#define EXTRACT_ENUM_(x) #x
-
-//We need a second macro function to get the proper behavior
-#define ENUM_TO_STRING(e) EXTRACT_ENUM_(e)
-
-#define LOG_ENUM(a) fprintf(MSGLOG, "%s", ENUM_TO_STRING(a))
-
-//The CASE_ENUM macro function is intended to be used within switch statements.
-//
-//The CASE_ENUM macro is based off of an example written by Peter Dalton, Evans & Sutherland
-//as section 1.2 in the book Game Programming Gems 2 (2001). Their example can be found on
-//page 19. 
-#define CASE_ENUM(val)       case(val) : LOG_ENUM( #val )     
-						      
-
-//Then a switch statement with enums might look like:
-//		switch (enum) {
-//		CASE_ENUM(ENUM1);
-//			processCaseEnum1();
-//			break;
-//		CASE_ENUM(ENUM2);
-//			processCaseEnum2();
-//			break;
-//
-//		//etc...
-//
-//		}
 
 #endif //LOGGING_MESSAGE_TARGET_CONFIGURATION_H_
