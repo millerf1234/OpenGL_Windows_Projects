5ec1e1f95a46dc74fcc35bf30f63c61e4a801a95
Forrest S Miller <4estmiller@comcast.net>
2019-09-28 22:18:26 -0700

    Fixed a few more typos

--- OpenGL_GLFW_Project/ImageData_UByte.h
+++ OpenGL_GLFW_Project/ImageData_UByte.h
@@ -213,7 +213,7 @@ public:
         GLsizei comp; //Number of components per pixel
         //Returns the number of bytes in total are expected
         //in an image matching these attributes
-        GLsizei sizeInBytes() const volatile noexcept {
+        GLsizei sizeInBytes() const noexcept {
             return (width * height * comp);
         }
         //Construct image attributes
