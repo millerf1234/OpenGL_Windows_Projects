3128966712b2beaffec084fc724c0b781dddd533
Forrest S Miller <4estmiller@comcast.net>
2019-10-09 01:28:16 -0700

    Added a note explaining that my specific desktop computer that I use for development has had a setting enabled within its GPU manufactuers configuration tool (it's within "NVidia Control Panel"). This setting opens up access to GPU performance counters to all users. By default they are only available for access if process has administrative permission.

--- OpenGL_GLFW_Project/Profiler.h
+++ OpenGL_GLFW_Project/Profiler.h
@@ -1,4 +1,7 @@
 
+//Please Note: On My Computer I have an NVidia GPU, and I have gone into
+//the NVidia control pannel and under the section titled 'developer' I have
+//enabled GPU performance counter access to all users. 
 
 #pragma once
 
