522f3af1b946b5d052078b0fab188d18bdf05d6a
Forrest S Miller <4estmiller@comcast.net>
2019-11-30 19:37:50 -0800

    Rewrote the 'catch-all' statement to catch any exceptions that may accidentally propogate all the way out to main

--- OpenGL_GLFW_Project/main.cpp
+++ OpenGL_GLFW_Project/main.cpp
@@ -18,19 +18,26 @@
 //  [see FailMalloc: http://www.nongnu.org/failmalloc/ ]
 #define SAFETY   try {
 #define FIRST } catch (const std::bad_alloc& e) {                              \
-fprintf(stderr, "\nWoah! Grab a camera. You have just experienced \n"          \
-    "the ultra rare, wildly elusive \'std::bad_alloc\'\n"                      \
-    "exception! This is the stuff you only read about\n"                       \
-    "these days in textbooks! Perhaps you should restart\n"                    \
-    "your computer, but congratulations on the\n"                              \
-    "new story you have for your grandchildren.\n"                             \
-    "\n\t[Here is the obligatory exception message]\n\t%s\n\n",                \
-    e.what());                                                                 \
+    fprintf(stderr, "\nWoah! Grab a camera. You have just experienced \n"      \
+        "the ultra rare, wildly elusive \'std::bad_alloc\'\n"                  \
+        "exception! This is the stuff you only read about\n"                   \
+        "these days in textbooks! Perhaps you should restart\n"                \
+        "your computer, but congratulations on the\n"                          \
+        "new story you have for your grandchildren.\n"                         \
+        "\n\t[Here is the obligatory exception message]\n\t%s\n\n",            \
+        e.what());                                                             \
 } catch (...) {                                                                \
-fprintf(stderr, "\n\n\n\nOH NO!\n\tAn Unexpected exception of unknown\n"       \
-    "origin was allowed to propogate all the way out to 'main()'!\n"           \
-    "This is very unexpected! Consult the developer please right away\n"       \
-    "if you have encountered this error!\n");                                  \
+    for (auto i = 0; i < 23; i++) { fprintf(stderr, "\n"); }                   \
+    fprintf(stderr, "\a ");                                                    \
+    for (auto i = 0; i < 77; i++) { fprintf(stderr, "!"); }                    \
+    fprintf(stderr, "\n\n\t\t\t\t   OH NO!\n"                                  \
+        "\t   An Unexpected exception of unknown origin was allowed to\n"      \
+        "\t   propagate all the way out to 'main()'!\n"                        \
+        "\t   This is very unexpected! Consult the developer please right\n"   \
+        "\t   away if you have encountered this issue!\n\n");                  \
+    fprintf(stderr, " ");                                                      \
+    for (auto i = 0; i < 77; i++) { fprintf(stderr, "!"); }                    \
+    fprintf(stderr, "\n\n\n\n");                                               \
 }
 
 
@@ -38,7 +45,7 @@ fprintf(stderr, "\n\n\n\nOH NO!\n\tAn Unexpected exception of unknown\n"       \
 
 
 
-int main(int argc, char * argv[]) {
+int main(/*int argc, char * argv[]*/) {
     SAFETY
     std::unique_ptr<Application> app = std::make_unique<Application>(); 
     app->launch();
